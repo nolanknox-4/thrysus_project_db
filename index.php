1b32a1a376ef2396a7dc5117a75b1f35f6c7362e
Charlotte <nolanknox4@gmail.com>
2026-02-04 23:01:04 -0600

    Initial clean commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>ACCESS GRANTED</h1>";
+echo "<p>Welcome to the Thrysus Project Database! A Scientific study of a world away from home.<p/>";
+echo "<a href= 'phyla_list.php'>ACCESS PHYLA RECORDS</a>";
---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO phyla (phylum_name, description) VALUES
+('Malakokelia', 'Softbodied invertebrates with radula derrived mouthparts, segmented bodies and jointed appendages.'),
+('Aspidossia', 'Endoskeletal vertebrae analogues, they have radial brains and silica bones.'),
+('Rotapodia', 'Exoskeletal invertebrates that evolved from a radial bodyplan, they have silica-argonite composite exoskeltons, and saclungs.');
